c08e30a3fcbf23ef6b9529188a042e7e3e65f7af
AleksaHrevtsova <aleksandra.hrevtsova@gmail.com>
2020-12-18 21:27:29 +0200

    LESSON-12

---
+++ lesson-12/js/index.js
@@ -0,0 +1,18 @@
+// show password by icon
+const passInput = document.getElementById('loginPass');
+const iconPassLink = document.querySelector('.iconPassLink');
+
+console.log(passInput, iconPassLink);
+
+function showPass(e) {
+  e.preventDefault();
+  if (passInput.type === 'password') {
+    iconPassLink.classList.add('show');
+    passInput.type = 'text';
+  } else {
+    iconPassLink.classList.remove('show');
+    passInput.type = 'password';
+  }
+}
+iconPassLink.addEventListener('click', showPass);
+// alert(`HELLO WORLD`);
